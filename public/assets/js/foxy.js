8fce38916e14b2286e5610213c038b879396a9f7
insert <94934670+slqntdevss@users.noreply.github.com>
2025-08-26 00:16:28 -0400

    more games

--- public/assets/js/foxy.js
+++ public/assets/js/foxy.js
@@ -1,14 +1,14 @@
 document.addEventListener("click", () => {
-	if (Math.random() < 0.0001) {
+	if (Math.random() < 0.5) {
 		const screen = document.createElement("div");
 		screen.className = "surprise";
 
 		const pic = document.createElement("img");
-		pic.src = "assets/getscared.gif";
+		pic.src = "/assets/img/getscared.gif";
 		pic.className = "surprisemf";
 
-		const sound = new Audio("assets/imaginegettingjumpscared.mp3");
-		sound.volume = 1.0;
+		const sound = new Audio("/assets/audio/imaginegettingjumpscared.mp3");
+		sound.volume = 2.0;
 		sound.play();
 
 		screen.appendChild(pic);
