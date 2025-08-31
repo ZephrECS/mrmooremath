8c1cd1810862a3d0aaaeddf7850f0bc8968139e7
slqnt <94934670+slqntdevss@users.noreply.github.com>
2025-08-30 20:21:49 -0400

    more gfames

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -2,7 +2,13 @@ const cursorSpeedSlider = document.getElementById("cursorSpeed");
 const currentSpeed = document.getElementById("currentSpeed");
 
 cursorSpeedSlider.addEventListener("input", (e) => {
-	console.log(e.target.value);
-	currentSpeed.textContent = e.target.value;
-	localStorage.setItem("cursorSpeed", e.target.value);
+  console.log(e.target.value);
+  currentSpeed.textContent = e.target.value;
+  localStorage.setItem("cursorSpeed", e.target.value);
+});
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("cursorSpeed") != null) {
+    currentSpeed.textContent = localStorage.getItem("cursorSpeed");
+    cursorSpeedSlider.value = localStorage.getItem("cursorSpeed");
+  }
 });
