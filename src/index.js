63df92aefc7718e6147cd0172d73fc51509777b5
insert <94934670+slqntdevss@users.noreply.github.com>
2025-09-02 21:06:11 -0400

    rage emoji

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 		);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
