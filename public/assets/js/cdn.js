6b1948064e03e7fa709aab62cdca2928ad2fc5b3
insert <94934670+slqntdevss@users.noreply.github.com>
2025-10-04 15:32:07 -0400

    update

--- public/assets/js/cdn.js
+++ public/assets/js/cdn.js
@@ -1,10 +1,12 @@
-const select = document.getElementById('cdn-switcher')
-const saved = localStorage.getItem('cdnBase') || 'https://cdn.statically.io/gl/3kh0/3kh0-assets/main/'
-select.value = saved
+const select = document.getElementById("cdn-switcher");
+const saved =
+	localStorage.getItem("cdnBase") ||
+	"https://cdn.statically.io/gl/3kh0/3kh0-assets/main/";
+select.value = saved;
 
-select.addEventListener('change', e => {
-  let val = e.target.value
-  if (!val.endsWith('/')) val += '/'
-  localStorage.setItem('cdnBase', val)
-  location.reload()
-})
+select.addEventListener("change", (e) => {
+	let val = e.target.value;
+	if (!val.endsWith("/")) val += "/";
+	localStorage.setItem("cdnBase", val);
+	location.reload();
+});
