e9992486244bc77d929fcfd57260aae0ae324b80
insert <94934670+slqntdevss@users.noreply.github.com>
2025-10-09 20:19:25 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ app.use(cors());
 
 app.use((req, res, next) => {
 	res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
 	next();
 });
 
