4f79f6b235dcf93ea86270119682ceb19a1ddd2a
insert <94934670+slqntdevss@users.noreply.github.com>
2025-10-13 10:04:45 -0400

    wowowowow

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
 	res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+	res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
 	next();
 });
 
